d3924d4a4c3c5cb7af92d438f248a4c2c001a6f0
dimansipl <dmitry.osipov2000@mail.ru>
2022-02-13 13:24:59 +0300

    add class Family_tree

--- WidthFormatting/FAMILY_TREE.h
+++ WidthFormatting/FAMILY_TREE.h
@@ -3,3 +3,15 @@
 #pragma once
 #define MAXMAMBNAMB 50
 #define MAXB        1024
+/**  Класс семейного древа
+   child Ц ребенок
+   parent Ц  родитель
+   /param[knee] - колено
+*/
+class FAMILY_TREE
+{
+public:
+	char parent[MAXMAMBNAMB];
+	char child[MAXMAMBNAMB];
+	int knee; // колено
+};
