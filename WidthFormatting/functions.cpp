6e43ea359fa950a62b8bbda0714bf9c136b9fc35
dimansipl <dmitry.osipov2000@mail.ru>
2022-02-16 19:28:23 +0300

    add function analyze_input

--- WidthFormatting/functions.cpp
+++ WidthFormatting/functions.cpp
@@ -6,4 +6,11 @@
 #include<fstream>
 #include "FAMILY_TREE.h"
 
-
+bool analyse_input(char* buffer, char** child, char** parent)
+{
+	//Анализ входных данных
+	*child = strtok(buffer, "\t ");
+	*parent = strtok(NULL, "\t ");
+	//Возвращаем наличие/отсутствие входных данных
+	return(*child && *parent) ? true : false;
+}
